a9459f4a7ea4f4de7065dc771c2821417508b3dd
melevir <melevir@gmail.com>
2016-09-08 13:17:33 +0300

    boilerplate added

---
+++ coursera.py
@@ -0,0 +1,16 @@
+
+
+def get_courses_list():
+    pass
+
+
+def get_course_info(course_slug):
+    pass
+
+
+def output_courses_info_to_xlsx(filepath):
+    pass
+
+
+if __name__ == '__main__':
+    pass
